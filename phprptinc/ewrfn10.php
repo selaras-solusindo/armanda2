4bea15c05b07ff46c942f6bb8d54dd021108da2b
selaras-solusindo <selusin.info@gmail.com>
2017-01-31 16:45:39 +0700

    database connection for php report maker

--- phprptinc/ewrfn10.php
+++ phprptinc/ewrfn10.php
@@ -6740,6 +6740,17 @@ function Database_Connecting(&$info) {
 	// $info["db"] = "production_db";
 	// }
 
+	if (ew_CurrentUserIP () == "127.0.0.1"  || ew_CurrentUserIP () == "::1"  || ew_CurrentHost () == "localhost" ) { // testing on local PC
+		$info["host"] = "localhost";
+		$info["user"] = "root"; // sesuaikan dengan username database di komputer localhost
+		$info["pass"] = "admin"; // sesuaikan dengan password database di komputer localhost
+		$info["db"] = "db_armanda2"; // sesuaikan dengan nama database di komputer localhost
+	} else { // setting koneksi database untuk komputer server
+		$info["host"] = "mysql.idhostinger.com";  // sesuaikan dengan ip address atau hostname komputer server
+		$info["user"] = "u485477426_arman"; // sesuaikan dengan username database di komputer server
+		$info["pass"] = "PresarioCQ43"; // sesuaikan deengan password database di komputer server
+		$info["db"] = "u485477426_arman"; // sesuaikan dengan nama database di komputer server
+	}
 }
 
 // Database Connected event
