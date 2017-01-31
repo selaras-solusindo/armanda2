4e368ac1959a0ef240046e6d7b55a65604649499
selaras-solusindo <selusin.info@gmail.com>
2017-01-31 16:33:04 +0700

    conn.php

--- conn.php
+++ conn.php
@@ -9,8 +9,8 @@ if ($_SERVER["REMOTE_ADDR"] == "127.0.0.1"  || $_SERVER["REMOTE_ADDR"] == "::1" 
 }
 else {
 	$hostname_conn = "mysql.idhostinger.com";
-	$database_conn = "u814357398_arman"; //$database_conn = "zecorind_mitra2";
-	$username_conn = "u814357398_arman"; //$username_conn = "zecorind_root";
+	$database_conn = "u485477426_arman"; //$database_conn = "zecorind_mitra2";
+	$username_conn = "u485477426_arman"; //$username_conn = "zecorind_root";
 	$password_conn = "PresarioCQ43";
 }
 ?>
